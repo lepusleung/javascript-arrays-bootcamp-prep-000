6beb9a52b57d9bdfa4eb1dbc6b486e6ee0f731f1
Man Leung <lepusleung@gmail.com>
2017-09-20 15:34:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,41 +1,40 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"]
-
-function addElementToBeginningOfArray(array,element){
-  return [element, ...array]
-
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+function addElementToBeginningOfArray(array, element) {
+  return [element,...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(array,element){
-   array.unshift(element);
-    return array
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
 }
 
-function addElementToEndOfArray(array,element){
-  return [...array, element]
+function addElementToEndOfArray(array, element) {
+  return [...array,element];
 }
 
-function destructivelyAddElementToEndOfArray(array,element){
-  array.push(element)
-  return array
-
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
 }
 
-function accessElementInArray(array, index){
-  return array[index]
+function accessElementInArray(array, index) {
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+function removeElementFromBeginningOfArray(array) {
+  newArray = array.slice(1);
+  return newArray;
 }
 
-function removeElementFromBeginningOfArray(array){
-  return array[index]
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
 }
 
-function removeElementFromEndOfArray(array){
-  return array[index]
-}
-
-function removeElementFromBeginningOfArray(array){
- return array.slice(1)
-}
-
-function removeElementFromEndOfArray(array){
-  return array.slice(0,array.length-1)
+function removeElementFromEndOfArray(array) {
+  newArray = array.slice(0,-1);
+  return newArray;
 }
